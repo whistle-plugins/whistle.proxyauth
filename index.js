a0029abf8d89f0a218efcc7a711c27e5e9c5ce1e
avwo <avwu@qq.com>
2021-11-17 15:00:57 +0800

    refactor: 改用 auth hook 代替 rulesServer

--- index.js
+++ index.js
@@ -3,50 +3,36 @@ const LRU = require('lru-cache');
 const AUTH_RE = /^\s*basic\s+(\S+)/i;
 const MAX_AGE = 1000 * 60 * 60;
 const MAX_AGE_RE = /@\d+$/;
-const cache = new LRU({
-  max: 10000,
-  maxAge: MAX_AGE,
-});
-const ERR_RULES = JSON.stringify({
-  rules: '/./ statusCode://401 file://{__authFaildTips}',
-  values: {
-    __authFaildTips: 'Access denied, please <a href="javascript:;" onclick="location.reload()">try again</a>.',
-  },
-});
+const cache = new LRU({ max: 10000, maxAge: MAX_AGE });
 
-const decodeBase64 = (str) => {
-  try {
-    return Buffer.from(str, 'base64').toString();
-  } catch (e) {}
-  return '';
-};
-// 相当于 rulesServer + tunnelRulesServer
-exports.pluginRulesServer = (server) => {
-  server.on('request', (req, res) => {
-    let {
-      ruleValue,
-      clientIp,
-    } = req.originalReq;
-    if (!ruleValue) {
-      return res.end();
+const checkAuth = (auth, value) => {
+  if (!auth || typeof auth !== 'string') {
+    return false;
+  }
+  auth = auth.substring(auth.indexOf(' ')).trim();
+  return auth && Buffer.from(str, 'base64').toString() === value;
+}
+
+exports.auth = (req, res) => {
+  let { ruleValue, clientIp } = req.originalReq;
+  let maxAge = MAX_AGE;
+  if (ruleValue && MAX_AGE_RE.test(ruleValue)) {
+    const time = RegExp['$&'];
+    maxAge = Math.max(+time.substring(1), 60000);
+    ruleValue = ruleValue.slice(0, -time.length);
+  }
+  if (!ruleValue) {
+    return;
+  }
+  const authKey = `${ruleValue}\n${clientIp}`;
+  if (!cache.get(authKey)) {
+    const { authorization: auth1, 'proxy-authorization': auth2 } = req.headers;
+    if (!checkAuth(auth1, ruleValue) || (decodeBase64(RegExp.$1) !== ruleValue)) {
+      req.setLogin(true);
+      req.setHtml('Access denied, please <a href="javascript:;" onclick="location.reload()">try again</a>.');
+      return false;
     }
-    let maxAge = MAX_AGE;
-    if (MAX_AGE_RE.test(ruleValue)) {
-      const time = RegExp['$&'];
-      maxAge = Math.max(+time.substring(1), 60000);
-      ruleValue = ruleValue.slice(0, -time.length);
-      if (!ruleValue) {
-        return res.end();
-      }
-    }
-    const authKey = `${ruleValue}\n${clientIp}`;
-    if (!cache.get(authKey)) {
-      if (!AUTH_RE.test(req.headers.authorization) ||
-          (decodeBase64(RegExp.$1) !== ruleValue)) {
-        return res.end(ERR_RULES);
-      }
-    }
-    cache.set(authKey, 1, maxAge);
-    return res.end();
-  });
+  }
+  cache.set(authKey, 1, maxAge);
+  return res.end();
 };
